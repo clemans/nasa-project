7f9aaeda64de3bddc2aa45cfc5c6df61f542d420
Brooks C. Clemans <brooks@clemans.net>
2025-05-09 22:57:22 -0400

    Adds testing for launches route.

--- server/src/routes/launches/launches.test.mjs
+++ server/src/routes/launches/launches.test.mjs
@@ -11,10 +11,49 @@ describe('GET /launches', () => {
 });
 
 describe('POST /launch', () => {
-    test('It should resond with 201 success', async () => {
 
+    const data = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 17071-D',
+        target: 'Kepler-186 f',
+        launchDate: 'January 4, 2028',
+    }
+
+    test('It should resond with 201 success', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send(data)
+        .expect('Content-Type', /json/)
+        .expect(201);
+
+        expect(response.body).toMatchObject({
+            ...data,
+            launchDate: new Date(data.launchDate).toISOString(),
+        });
+    
     });
 
-    test('It should catch missing required properties', () => {});
-    test('It should catch invalid dates', () => {});
+    test('It should catch missing required properties', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send({ ...data, launchDate: ''})
+        .expect('Content-Type', /json/)
+        .expect(400);
+
+        expect(response.body).toStrictEqual({
+            error: 'Invalid or missing property input.'
+        });
+    });
+
+    test('It should catch invalid dates', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send({ ...data, launchDate: 'HelloWorld'})
+        .expect('Content-Type', /json/)
+        .expect(400);
+
+        expect(response.body).toStrictEqual({
+            error: 'Invalid Date input'
+        })
+    });
 });
